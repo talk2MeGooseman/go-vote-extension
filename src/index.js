b51c304b0139c8ef3258d6032abcd62361b4c65f
Erik Guzman <erik.guzman@guzman.codes>
2020-09-23 04:36:53 +0000

    Update all deps and fix things up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
-import isToday from 'date-fns/is_today'
-import isBefore from 'date-fns/is_before'
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
+import isToday from 'date-fns/isToday'
+import isBefore from 'date-fns/isBefore'
+import distanceInWordsToNow from 'date-fns/formatDistance'
 import './styles.scss';
 
 const ELECTION_DAY = "2020-11-3";
@@ -10,6 +10,10 @@ const electionDate = new Date(ELECTION_DAY);
 const momentDate = moment(electionDate);
 const isElectionDay = isToday(electionDate);
 const isBeforeElection = isBefore(new Date(), electionDate);
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 window.onload = function() {
   let twitchAuth, settings;
@@ -25,15 +29,9 @@ window.onload = function() {
   if (isElectionDay)
   {
     countDownEl.innerHTML = "Elections Are Today, " + momentDate.format('MMM. D, YYYY');
-  } else if (isBeforeElection)
-  {
-    countDownEl.innerHTML = `${distanceInWordsToNow(electionDate)} until ${momentDate.format('MMM. D, YYYY')} elections`;
-  } else
-  {
+  } else if (isBeforeElection) {
+    countDownEl.innerHTML = `${capitalize(distanceInWordsToNow(electionDate, Date.now()))} until ${momentDate.format('MMM. D, YYYY')} elections`;
+  } else {
     countDownEl.innerHTML = "Thanks for Voting!!";
   }
-
-  // bodyEl.onclick = () => {
-  //   window.open('https://vote.gov/')
-  // }
 };
