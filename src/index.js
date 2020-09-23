8fa4d69f1db5951f3c11c80ff5b7ab7e835cac49
Erik Guzman <erik.guzman@guzman.codes>
2020-09-22 22:33:52 -0700

    Update styles for panel usage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import moment from 'moment';
-import isToday from 'date-fns/isToday'
-import isBefore from 'date-fns/isBefore'
-import distanceInWordsToNow from 'date-fns/formatDistance'
+import isToday from 'date-fns/isToday';
+import isBefore from 'date-fns/isBefore';
+import distanceInWordsToNow from 'date-fns/formatDistance';
+import format from 'date-fns/format';
 import './styles.scss';
 
-const ELECTION_DAY = "2020-11-3";
+const ELECTION_DAY = '2020-11-3';
 
 const electionDate = new Date(ELECTION_DAY);
-const momentDate = moment(electionDate);
+const formattedElectionDay = format(electionDate, 'PP');
 const isElectionDay = isToday(electionDate);
 const isBeforeElection = isBefore(new Date(), electionDate);
 
@@ -15,23 +15,24 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-window.onload = function() {
+window.onload = function () {
   let twitchAuth, settings;
   let countDownEl = document.querySelector('.count-down');
   // let bodyEl = document.querySelector('body');
 
   // Listen for auth token event
   window.Twitch.ext.onAuthorized(async (auth) => {
-      // On Twitch Auth fetch Channel Setting information
+    // On Twitch Auth fetch Channel Setting information
   });
 
   // video_component settings
-  if (isElectionDay)
-  {
-    countDownEl.innerHTML = "Elections Are Today, " + momentDate.format('MMM. D, YYYY');
+  if (isElectionDay) {
+    countDownEl.innerHTML = 'Elections Are Today, ' + formattedElectionDay;
   } else if (isBeforeElection) {
-    countDownEl.innerHTML = `${capitalize(distanceInWordsToNow(electionDate, Date.now()))} until ${momentDate.format('MMM. D, YYYY')} elections`;
+    countDownEl.innerHTML = `${capitalize(
+      distanceInWordsToNow(electionDate, Date.now())
+    )} until ${formattedElectionDay} elections`;
   } else {
-    countDownEl.innerHTML = "Thanks for Voting!!";
+    countDownEl.innerHTML = 'Thanks for Voting!!';
   }
 };
